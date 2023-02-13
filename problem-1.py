45810f32a987de29e30ec7250885891b80b179cf
MacBook <macbook@MacBooks-MacBook-Pro-2.local>
2023-02-13 11:49:05 +0530

    Problem-Statement-01

---
+++ problem-1.py
@@ -0,0 +1,16 @@
+'''Write a program which basically sum all the integers from the sample_input
+string and returns the final result.Sample_input =”PRA12Ga782d45601$3”'''
+
+#Enter Any String
+string=input()
+
+# Function to sum integers present in String
+def stringsum(string):
+    sum=0
+    for i in range(len(string)):
+        if string[i].isnumeric():
+            sum+=int(string[i])
+    return sum
+
+# Calling Function to Find Result
+print(stringsum(string))
